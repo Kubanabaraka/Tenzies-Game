f47e381a82411da222bb666f7ac9c2919ef99af7
kubanabaraka <byukusengekubanabarakapascal@gmail.com>
2026-04-19 09:21:28 +0200

    Refactor dice generation to include id and isHeld properties for each die

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./Die";
 
 export default function App() {
@@ -14,14 +15,18 @@ export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
   const rollDice = () => {
     setDice(generateAllNewDice());
   };
 
-  const diceElements = dice.map((num, index) => (
-    <Die key={index} value={num} />
+  const diceElements = dice.map((dieObj) => (
+    <Die key={dieObj.id} value={dieObj.value} />
   ));
 
   return (
